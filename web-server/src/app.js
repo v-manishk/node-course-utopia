79d9559debda9f35e92b4703c9e1bf2c870f23f1
v-manishk <vmanish1313@gmail.com>
2023-08-18 18:55:10 +0530

    day 3 end sec 7 lec 50 till advanced templating and 404 error

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,10 @@ const path = require('path')
 // we can call it create a new express application
 const express = require('express')   
 
+// to work with partials
+const hbs = require('hbs')
+
+
 // get the location of folder and file
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
@@ -15,12 +19,16 @@ const app = express()
 // define path for expressjs
 const publicDirectoryPath = path.join(__dirname, '../public')
 // can be use while views directory name is changes eg. template
-// const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+// path for partials
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // tells express which templation engine we are going to use (setup handlebars engines)
 app.set('view engine', 'hbs')
 // setup handlebars voew location
-// app.set('view', 'viewPath')
+app.set('views', viewPath)
+// for hbs partials
+hbs.registerPartials(partialsPath)
 
 
 // serving the public directory
@@ -46,7 +54,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: "Help",
-        msg: "Want help, send your questions to us, we will try to resolve it"
+        msg: "Want help, send your questions to us, we will try to resolve it",
+        name: "ManishKumar Vishwkarma"
     })
 })
 
@@ -54,6 +63,22 @@ app.get('/weather', (req, res) => {
     res.send({
         location: 'Mumbai',
         forecast: 32
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: "Error",
+        msg: "Help article not found",
+        name: "ManishKumar Vishwakarma"
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "Error",
+        msg: "404 page not found",
+        name: "ManishKumar Vishwakarma"
     })
 })
 
