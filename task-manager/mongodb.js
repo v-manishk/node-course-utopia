929b8bad1da40dc7e197d817419973b7200e6f8b
v-manishk <vmanish1313@gmail.com>
2023-08-22 15:06:19 +0530

    Day 6 Section 10 Lec 81 End i.e. end of the mongodb CRUD operation and we will start Mongoose after this commit   Updating the formated file of mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -31,80 +31,77 @@ const databaseName = 'task-manager'
 //     }
 // })
 
-
 const client = new MongoClient(connectionURL);
 
-
-
-
 async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
+
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+
+    const db = client.db(databaseName)
+
+    // const insertResult = await db.collection('task').insertOne({
+    //     _id: id,
+    //     description: 'Mnaihs',
+    //     completed: false
+    // })
+    // // const insertResult = await db.collection('task').insertMany([{ description: 'Complete this nodejs tutorial', completed: false }, { description: 'finish backend course', completed: false }, {description: 'finish basic java', completed: true}]);
+    // console.log('Inserted documents =>', insertResult);
+
+    // const findResult = await db.collection('task').countDocuments({ completed: false });
+    // console.log('Found documents => ', findResult)
+
+    // await db.collection('documents').insertMany([
+    //     {
+    //         name: 'axy',
+    //         age: 30
+    //     }, {
+    //         name: 'asd',
+    //         age: 30
+    //     }
+    // ]).then((result) => {
+    //     console.log('Success!', result)
+    // }).catch((error) => {
+    //     console.log('Error!', error)
+    // })
 
 
-  const db = client.db(databaseName)
-  // const insertResult = await db.collection('task').insertOne({
-  //   _id: id,
-  //   description: 'Mnaihs',
-  //   completed: false
-  // })
-  // // const insertResult = await db.collection('task').insertMany([{ description: 'Complete this nodejs tutorial', completed: false }, { description: 'finish backend course', completed: false }, {description: 'finish basic java', completed: true}]);
-  // console.log('Inserted documents =>', insertResult);
+    await db.collection('documents').deleteMany({
+        age: 30
+    }).then((result) => {
+        console.log('Success!', result)
+    }).catch((error) => {
+        console.log('Error!', error)
+    })
 
-  // const findResult = await db.collection('task').countDocuments({ completed : false});
-  // console.log('Found documents => ', findResult)
+    // await db.collection('documents').updateOne({
+    //     name: 'Ram'
+    // }, {
+    //     $inc: {
+    //         age: -2
+    //     }
+    // }).then((result) => {
+    //     console.log('Updated:', result)
+    // }).catch((error) => {
+    //     console.log('Error!', error)
+    // })
 
-  // await db.collection('documents').insertMany([
-  //   {
-  //     name: 'axy',
-  //     age: 30
-  //   }, {
-  //     name: 'asd',
-  //     age: 30
-  //   }
-  // ]).then((result) => {
-  //   console.log('Success!', result)
-  // }).catch((error) => {
-  //   console.log('Error!', error)
-  // })
+    // await db.collection('documents').updateMany({
+    // }, {
+    //     $inc: {
+    //         age: +2
+    //     }
+    // }).then((result) => {
+    //     console.log('Updated:', result)
+    // }).catch((error) => {
+    //     console.log('Error!', error)
+    // })
 
-  await db.collection('documents').deleteMany({
-    age: 30
-  }).then((result) => {
-    console.log('Success!', result)
-  }).catch((error) => {
-    console.log('Error!', error)
-  })
-
-  // await db.collection('documents').updateOne({
-  //   name: 'Ram'
-  // }, {
-  //   $inc: {
-  //     age: -2
-  //   }
-  // }).then((result) => {
-  //   console.log('Updated:', result)
-  // }).catch((error) => {
-  //   console.log('Error!', error)
-  // })
-
-  // await db.collection('documents').updateMany({
-  //   }, {
-  //     $inc: {
-  //       age: +2
-  //     }
-  //   }).then((result) => {
-  //     console.log('Updated:', result)
-  //   }).catch((error) => {
-  //     console.log('Error!', error)
-  //   })
-
-
-  return 'done.';
+    return 'done'
 }
 
 main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
