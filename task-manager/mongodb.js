34440dcb3d259eafe95f80b3c40d3273fe5c7563
v-manishk <vmanish1313@gmail.com>
2023-08-22 14:57:27 +0530

    Day 6 Section 10 Lec 81 End i.e. end of the mongodb CRUD operation and we will start Mongoose after this commit

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,11 +3,12 @@
 // const MongoClient = mongodb.MongoClient
 // const ObjectId = mongodb.ObjectId 
 
-const { MongoClient, ObjectId, Collection } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 // console.log('Hello')
+
 // MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 //     console.log('Hello1')
 
@@ -17,7 +18,7 @@ const databaseName = 'task-manager'
 
 //     // console.log('Connected')
 
-//     const db = client.db(databaseName)
+    // const db = client.db(databaseName)
 //     db.collection('documents').inserOne({
 //         name: 'Manish',
 //         age: 22
@@ -30,35 +31,77 @@ const databaseName = 'task-manager'
 //     }
 // })
 
-// const id = new ObjectId()
-// console.log(id.toString())
-// console.log(id.getTimestamp())
-// console.log(id.id.length)
-// console.log(id.toHexString().length)
 
 const client = new MongoClient(connectionURL);
 
 
+
+
 async function main() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    
-    
-    const db = client.db(databaseName)
-    // const insertResult = await db.collection('task').insertOne({
-    //   _id: id,
-    //   description: 'Mnaihs',
-    //   completed: false
-    // })
-    // // const insertResult = await db.collection('task').insertMany([{ description: 'Complete this nodejs tutorial', completed: false }, { description: 'finish backend course', completed: false }, {description: 'finish basic java', completed: true}]);
-    // console.log('Inserted documents =>', insertResult);
-
-    const findResult = await db.collection('task').countDocuments({ completed : false});
-    console.log('Found documents => ', findResult)
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
 
 
-    return 'done.';
+  const db = client.db(databaseName)
+  // const insertResult = await db.collection('task').insertOne({
+  //   _id: id,
+  //   description: 'Mnaihs',
+  //   completed: false
+  // })
+  // // const insertResult = await db.collection('task').insertMany([{ description: 'Complete this nodejs tutorial', completed: false }, { description: 'finish backend course', completed: false }, {description: 'finish basic java', completed: true}]);
+  // console.log('Inserted documents =>', insertResult);
+
+  // const findResult = await db.collection('task').countDocuments({ completed : false});
+  // console.log('Found documents => ', findResult)
+
+  // await db.collection('documents').insertMany([
+  //   {
+  //     name: 'axy',
+  //     age: 30
+  //   }, {
+  //     name: 'asd',
+  //     age: 30
+  //   }
+  // ]).then((result) => {
+  //   console.log('Success!', result)
+  // }).catch((error) => {
+  //   console.log('Error!', error)
+  // })
+
+  await db.collection('documents').deleteMany({
+    age: 30
+  }).then((result) => {
+    console.log('Success!', result)
+  }).catch((error) => {
+    console.log('Error!', error)
+  })
+
+  // await db.collection('documents').updateOne({
+  //   name: 'Ram'
+  // }, {
+  //   $inc: {
+  //     age: -2
+  //   }
+  // }).then((result) => {
+  //   console.log('Updated:', result)
+  // }).catch((error) => {
+  //   console.log('Error!', error)
+  // })
+
+  // await db.collection('documents').updateMany({
+  //   }, {
+  //     $inc: {
+  //       age: +2
+  //     }
+  //   }).then((result) => {
+  //     console.log('Updated:', result)
+  //   }).catch((error) => {
+  //     console.log('Error!', error)
+  //   })
+
+
+  return 'done.';
 }
 
 main()
